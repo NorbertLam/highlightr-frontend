6414f8167ca11075cc27a6800e41e0778cb6b1ae
NorbertLam <noobertlm@gmail.com>
2019-04-04 15:29:05 -0400

    add vod functionality

---
+++ src/containers/VodCardContainer.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import VodCard from '../components/VodCard'
+
+import Grid from '@material-ui/core/Grid';
+
+class VodCardContainer extends React.Component {
+
+  state = {
+    vods: []
+  }
+
+  componentWillReceiveProps(nextProps) {
+    fetch(`https://api.twitch.tv/helix/videos?user_id=${nextProps.twitch_id.toLowerCase()}`, {
+      headers: {
+        'Client-ID': ''
+      }
+    })
+      .then(resp => resp.json())
+      .then(json => this.setState({vods: json.data}));
+  }
+
+  render() {
+    const vodsArr = this.state.vods.map(vod => <VodCard key={vod.id} vodObj={vod} />)
+
+    return (
+      <div>
+        <h1>Vods</h1>
+        <Grid container>
+          {vodsArr}
+        </Grid>
+      </div>
+    )
+  }
+}
+
+export default VodCardContainer;
