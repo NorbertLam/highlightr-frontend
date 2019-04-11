33667fa8da0d6a87c958be0b54408f763e0dc7fc
NorbertLam <noobertlm@gmail.com>
2019-04-11 17:19:59 -0400

    made textfield white

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,9 +6,28 @@ import {connect} from 'react-redux';
 import MenuItem from '@material-ui/core/MenuItem';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
+import {withStyles} from "@material-ui/core/styles";
+import PropTypes from "prop-types";
 
 import {getSearch} from '../actions/searchActions';
 
+const styles = {
+  input: {
+    color: "white"
+  },
+  underline: {
+    color: "white",
+    '&:before': {
+      borderBottomColor: 'white',
+    },
+    '&:after': {
+      borderBottomColor: 'white',
+    },
+    '&:hover:before': {
+      borderBottomColor: ['white', '!important'],
+    }
+  }
+};
 
 class Search extends React.Component {
 
@@ -21,7 +40,14 @@ class Search extends React.Component {
     console.log('change');
   }
 
+  handleInput = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
   render() {
+    const { classes } = this.props;
+    console.log(classes);
+
     return (
       <div>
         <Downshift
@@ -41,7 +67,8 @@ class Search extends React.Component {
       }) => (
         <div>
           <label></label>
-          <TextField/>
+          {/* InputProps={{style: {color: 'white'}}} */}
+          <TextField InputProps={{classes:{underline: classes.underline}}} name='input' value={this.state.input} onChange={this.handleInput} />
         </div>
       )}
         </Downshift>
@@ -50,12 +77,16 @@ class Search extends React.Component {
   }
 }
 
+Search.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
 const mapStateToProps = (state) => {
-  results = state.results
+  return {results: state.results};
 }
 
 const mapDispatchToProps = (dispatch) => {
   return {getSearch: (term) => dispatch(getSearch(term))};
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Search);
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(Search));
